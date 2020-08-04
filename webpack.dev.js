62fa42aef7de29d930695abaf32ec92f3a447b08
tizee <33030965+tizee@users.noreply.github.com>
2020-08-04 22:17:27 +0800

    feat: add bundle analyzer

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,22 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+    .BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.join(__dirname, "dist"),
-  },
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, "dist"),
+        port: "8000",
+    },
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: "server",
+            analyzerHost: "127.0.0.1",
+            analyzerPort: "9527",
+        }),
+    ],
 });
